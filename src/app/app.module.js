6befabf1334a84c6de39620264d12e8060b0b141
amngupta <amangupta96@outlook.com>
2017-02-06 19:48:15 -0800

    Completed Tutorial of Quickstart

---
+++ src/app/app.module.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
+var hero_details_component_1 = require('./hero-details.component');
+var heros_component_1 = require('./heros.component');
+var heros_services_1 = require('./heros.services');
+var app_component_1 = require('./app.component');
+var dashboard_component_1 = require('./dashboard.component');
+var app_routing_module_1 = require('./app-routing.module');
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                app_routing_module_1.AppRoutingModule
+            ],
+            declarations: [app_component_1.AppComponent, hero_details_component_1.HeroDetailComponent, dashboard_component_1.DashboardComponent,
+                heros_component_1.HeroesComponent],
+            bootstrap: [app_component_1.AppComponent],
+            providers: [heros_services_1.HeroService],
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
